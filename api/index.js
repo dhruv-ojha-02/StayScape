6cc504f6c7d2b2582bfbdd06b0d44a9a12a1ceef
dhruv-ojha-02 <dhruvojha02@gmail.com>
2024-02-18 14:01:26 +0530

    login done

---
+++ api/index.js
@@ -0,0 +1,87 @@
+
+//MongoDB credentials
+//dhruvojha02
+//mImM3gSimfMT1Uye
+
+
+const express = require('express');
+const cors = require('cors');
+const mongoose = require("mongoose");
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('./models/User.js');
+const cookieParser = require('cookie-parser');
+require('dotenv').config()
+const app = express();
+
+const bcryptSalt = bcrypt.genSaltSync(10);
+const jwtSecret = 'f2bj32hv5j3h5bi5c83934cn';
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(cors({
+    // credentials: true,
+    origin: 'http://localhost:5173',
+    credentials: true,
+    optionsSuccessStatus: 200
+}));
+
+
+mongoose.connect(process.env.MONGO_URL);
+
+app.get('/test', (req, res) => {
+    res.json('test ok');
+});
+
+app.post('/register', async (req, res) => {
+    const { name, email, password } = req.body;
+
+    try {
+        const userDoc = await User.create({
+            name,
+            email,
+            password: bcrypt.hashSync(password, bcryptSalt),
+        });
+
+        res.json(userDoc);
+    }
+    catch (e) {
+        res.status(422).json(e);        //Status 422 is Unprocessable entity(for preventing Duplicate key error)
+    }
+});
+
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    const userDoc = await User.findOne({ email });
+    if (userDoc) {
+        const passOk = bcrypt.compareSync(password, userDoc.password);
+        if (passOk) {
+            jwt.sign({ email: userDoc.email, id: userDoc._id, name:userDoc.name}, jwtSecret, {}, (err, token) => {
+                if (err) throw err;
+                res.cookie('token', token).json(userDoc);
+            })
+        }
+        else {
+            res.status(422).json('pass not ok');
+        }
+    }
+    else {
+        res.json('not found');
+    }
+});
+
+app.get('/profile', (req, res) => {
+    const { token } = req.cookies;
+    if (token) {
+        jwt.verify(token, jwtSecret, {}, (err, user) => {
+            if (err) throw err;
+            res.json(user);
+        });
+    }
+    else {
+        res.json(null);
+    }
+});
+
+app.listen(4000);
