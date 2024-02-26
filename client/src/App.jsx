04b35f8e28cc2aa4bebd16b662985f9746441e2c
dhruv-ojha-02 <dhruvojha02@gmail.com>
2024-02-26 18:47:46 +0530

    place page done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,9 +6,13 @@ import Layout from "./Layout";
 import RegisterPage from './pages/RegisterPage';
 import axios from "axios";
 import { UserContextProvider } from './UserContext';
+import ProfilePage from './pages/ProfilePage';
+import PlacesPage from './pages/PlacesPage';
+import PlacesFormPage from './pages/PlacesFormPage';
+import PlacePage from './pages/PlacePage';
+
 
 axios.defaults.baseURL = 'http://localhost:4000';
-
 axios.defaults.withCredentials = true;
 
 function App() {
@@ -19,6 +23,11 @@ function App() {
           <Route index element={<IndexPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
+          <Route path="/account" element={<ProfilePage />} />
+          <Route path="/account/places" element={<PlacesPage />} />
+          <Route path="/account/places/new" element={<PlacesFormPage />} />
+          <Route path="/account/places/:id" element={<PlacesFormPage />} />
+          <Route path="/place/:id"  element ={<PlacePage />}/>
         </Route>
       </Routes>
     </UserContextProvider>
